e1db1f3f30b766a62cb9d38cca6143d3bd00794e
Jayanthi Balasubramani <jayanthibs@gmail.com>
2026-01-31 14:25:22 -0500

    created the structure of the project

---
+++ e-commerce-system/src/services/apiService.ts
@@ -0,0 +1,22 @@
+async function fetchApiData(){
+    try{
+
+    const response = await fetch("https://dummyjson.com/products");
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+    const data = await response.json();
+    console.log(data);
+    const products = data.products;
+
+    for(let product of products){
+        console.log("id:"+ product.id, "title:"+product.title)
+    }
+    console.log(products.length);
+
+    }
+    catch (e) {
+    console.log(e);
+  }
+}
+fetchApiData();
