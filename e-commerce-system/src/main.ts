b71379aea76885a404c12215a7cf1564cf5f4d3c
Jayanthi Balasubramani <jayanthibs@gmail.com>
2026-02-02 17:49:34 -0500

    added try/catch on main.ts

--- e-commerce-system/src/main.ts
+++ e-commerce-system/src/main.ts
@@ -2,9 +2,22 @@ import { Product } from "./models/Product.js";
 import { fetchAPIData, products } from "./services/apiService.js";
 import { calculateDiscount } from "./utils/discountCalculator.js";
 import { calculateTax } from "./utils/taxCalculator.js";
+import { NetworkError,DataError } from "./utils/errorHandler.js";
 
 async function main(){
+    try{
+
 await fetchAPIData();
+}
+    catch(error) {
+    if (error instanceof NetworkError) {
+      console.log("Network Error", error.message);
+    } else if (error instanceof DataError) {
+      console.log("Data Error", error);
+    } else {
+      console.error("Unknown Error:", error);
+    }
+  }
 
 for(const product of products){
     const item = new Product (product.id, product.title, product.category, product.price, product.discountPercentage );
